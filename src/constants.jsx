293fd0a804501270f49f4e58ea891f6d1ea64f59
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-09-13 15:52:38 -0500

    changes to route

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,4 +1,4 @@
-export const HOME_ROUTE = "/home";
+export const HOME_ROUTE = "/";
 export const ABOUT_ROUTE = "/about";
 export const PORTFOLIO_ROUTE = "/portfolio";
 export const CONTACT_ROUTE = "/contact";
