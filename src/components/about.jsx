5ffb10752fe0782449742bfedd796bd1925b22e4
JeffHauckJr <jefferyhauckjr@gmail.com>
2021-09-14 15:20:15 -0500

    Small Grammar changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,11 +11,11 @@ const AboutInfo = () => {
      I'll also play the occasional game of pickup baseball to get out the house. Having a good work/life balance is crucial in preventing burnout.
     `;
 
-  const professionalInfo = `An athlete turned salesman who was ready for a change. I Always had a love for technology but never knew how to take the leap.
+  const professionalInfo = `An athlete turned salesman who was ready for a change. I always had a love for technology but never knew how to take the leap.
     Before Covid I held various jobs as a salesman and Customer Service Rep. Although I enjoyed it, I knew it was not the right career path for me.
-    When everything shut down I decided it was time ti tak control make the jump. In January 2021 I enrolled in LSU's Online Coding Bootcamp Powered By Fullstack Academy.
-    I endured the stress and my passion for coding grew by the day. During the program I had the opportunity to learn from Great Instructors and was able to gain experience
-    in group environments. While I was able to Thrive in this program I would be lying if I said it was not stressful. It was. But, after completing that last project and
+    When everything shut down I decided it was time to takre control make the jump. In January 2021 I enrolled in LSU's Online Coding Bootcamp Powered By Fullstack Academy.
+    I endured the stress and my passion for coding grew by the day. During the program I had the opportunity to learn from great instructors and was able to gain experience
+    in group environments. While I was able to thrive in this program I would be lying if I said it was not difficult. It was. But, after completing that last project and
     Graduating in July of 2021, I knew I was prepared to take on the World with my newfound love of coding and skills I had learned.
     `;
 
